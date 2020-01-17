e775e72eed5fdf587cda9d37784677375332aed9
Javier Arce <jasearce@espol.edu.ec>
2020-01-17 11:24:00 -0500

    pantalla resultados

---
+++ app/src/main/java/com/example/amst_2ep_grupo11_zamora_arce/ResultadoActivity.java
@@ -0,0 +1,14 @@
+package com.example.amst_2ep_grupo11_zamora_arce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado);
+    }
+}
