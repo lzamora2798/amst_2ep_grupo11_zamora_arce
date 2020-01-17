318885a5b95dfc1c492406afe655c6709005867c
luis zamora <lzamora@espol.edu.ec>
2020-01-17 11:36:26 -0500

    libreria volley

--- app/src/main/java/com/example/amst_2ep_grupo11_zamora_arce/MainActivity.java
+++ app/src/main/java/com/example/amst_2ep_grupo11_zamora_arce/MainActivity.java
@@ -5,18 +5,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editText;
+    String Abuscar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        editText = (EditText) findViewById(R.id.editText);
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
     public void busqueda(View view){
-        //Intent intent = new Intent(this, );
+        Abuscar = editText.getText().toString();
+        //Volley.newRequestQueue("https://superheroapi.com/api/access-token/2961174840561847");
+        Intent intent = new Intent(this,ResultadoActivity.class );
+        intent.putExtra("busqueda",Abuscar);
+        startActivity(intent);
 
-        //intent.putExtra("ID","3432");
-        //startActivity(intent);
+
     }
 }
+
+/*
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registros);
+        setTitle("Grafico de barras");
+        temperaturasTVs = new HashMap<String,TextView>();
+        fechasTVs = new HashMap<String,TextView>();
+        ListaRequest = Volley.newRequestQueue(this);
+        contexto= this;
+        GRAFICO
+            this.iniciarGrafico();
+            this.solicitarTemperaturas(); }
+            */
