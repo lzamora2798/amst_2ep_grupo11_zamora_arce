932f7f4da270bcdc5e6d36a26ef275578047635c
luis zamora <lzamora@espol.edu.ec>
2020-01-17 12:08:54 -0500

    mas cambios

--- app/src/main/java/com/example/amst_2ep_grupo11_zamora_arce/ResultadoActivity.java
+++ app/src/main/java/com/example/amst_2ep_grupo11_zamora_arce/ResultadoActivity.java
@@ -5,41 +5,66 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ResultadoActivity extends AppCompatActivity {
 
     String abuscar;
+    RequestQueue ListaRequest = null;
+    String token  = "2961174840561847";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        Volley.newRequestQueue(this);
-        abuscar =getIntent().getStringExtra("ID");
 
-        String url = "http://my-json-feed";
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        //textView.setText("Response: " + response.toString());
-                    }}, new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // TODO: Handle error
-
-                    }
-                });
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
 
+        abuscar =getIntent().getStringExtra("busqueda");
+        ListaRequest = Volley.newRequestQueue(this);
+        buscarHeroe();
     }
+
+    public void buscarHeroe(){
+        String url_registros = "https://superheroapi.com/api/2961174840561847/search/"+abuscar;
+        JsonArrayRequest requestRegistros = new JsonArrayRequest(Request.Method.GET, url_registros, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                //mostrarTemperaturas(response);
+                //actualizarGrafico(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println(error);
+            }
+        }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> params = new HashMap<>();
+                params.put("Authorization", "JWT " + "2961174840561847");
+                return params;
+            }
+        };
+
+        ListaRequest.add(requestRegistros);
+
+    }
+    public void imprimir(){
+        System.out.println(ListaRequest.toString());
+
+    }
+
 }
